323ac11597e2ce3e74b84057542c867108222e37
akzarin <anynhaluiza10@gmail.com>
2022-07-13 16:47:25 -0300

    projeto inteiro

---
+++ cypress/cypress/e2e/3-alura/testealura.cy.js
@@ -0,0 +1,17 @@
+describe('Testar se funciona', () => {
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com/#/home');
+    })
+
+    it('Login valido', () => {
+        cy.login('flavio', '123')
+        cy.contains('a', '(Logout)').should('be.visible')
+    })
+
+    it('Login invalido', () => {
+        cy.login('flavio', '12345')
+        cy.on('window:alert', (str) => { //pra identificar que uma janela de alerta deve abrir e ter o conteudo abaixo
+            expect(str).to.equal('Invalid user name or password') //str = string 
+        })
+    })
+})
